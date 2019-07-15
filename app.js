5e3c182a2a161b2163490598a776cfccb795ad53
Waclaw Beletsky <hardhill@live.ru>
2019-07-15 23:58:44 +0800

    предпоследний этап

--- app.js
+++ app.js
@@ -13,20 +13,31 @@ const cars=[
     getCar("Toyota","Camry",2011,"Lord Hamlet","+7125 434 77 03","images/toyotacamry.jpg"),
     getCar("Porche","Panamera",2017,"Suzi Cone","+7984 200 72 66","images/porchepanamera.jpg")
 ]
+const log = (text, status, date = new Date()) => ({text, status, date});
 new Vue({
     el:'#app',
     data:{
         cars:cars,
         car: cars[0],
+        logs: [],
         selectedCarindex: 0,
         phoneVisibility: false,
-        searchtext: ''
+        searchtext: '',
+        modalVisibility: false
     },
     methods:{
         selectCar:function(index){
             this.car = cars[index];
             this.selectedCarindex = index;
             this.phoneVisibility = false;
+        },
+        newOrder: function () {
+            this.modalVisibility = false;
+            this.logs.push(log(`Success order:${this.car.name}-${this.car.model}`, 'ok'));
+        },
+        cancelOrder: function () {
+            this.modalVisibility = false;
+            this.logs.push(log(`Cancel order:${this.car.name}-${this.car.model}`, 'cancel'));
         }
     },
     computed: {
@@ -38,6 +49,11 @@ new Vue({
                 return item.name.toLowerCase().indexOf(this.searchtext.toLowerCase()) > -1 || item.model.toLowerCase().indexOf(this.searchtext.toLowerCase()) > -1;
             });
             return filtered;
+        }
+    },
+    filters: {
+        mydate(value) {
+            return value.toLocaleDateString();
         }
     }
 })
